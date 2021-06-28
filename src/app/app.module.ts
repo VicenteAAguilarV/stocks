929926e8c0b6c47e894ae1553bb2b64d9213a856
VicenteAAguilarV <86620783+VicenteAAguilarV@users.noreply.github.com>
2021-06-28 12:34:07 -0500

    3

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { StocksService } from './services/stocks.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+	HttpClientModule
+  ],
+  providers: [StocksService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
