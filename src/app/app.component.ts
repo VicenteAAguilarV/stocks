3696f85ae6f8aaae4a08bdc9ba499906f58ea6bb
VicenteAAguilarV <86620783+VicenteAAguilarV@users.noreply.github.com>
2021-06-28 13:19:02 -0500

    Se agregó el  componente Summary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { StocksService, StockInterface } from './services/stocks.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stocks';
+   // Declara una propiedad como un arreglo de acciones 
+ stocks: Array<StockInterface> = [];
+ constructor(service: StocksService) {
+ service.load(['AAPL']).subscribe(stocks => {
+ this.stocks = stocks;
+ });
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule
